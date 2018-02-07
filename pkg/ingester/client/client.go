fc0b8323240a10abf1f4f0825f6dfdbeef87115f
Ken Haines <khaines@ea.com>
2018-02-06 16:09:45 -0800

    adding config option & defaults for ingester client graph.MaxCallRecvMsgSize setting

--- pkg/ingester/client/client.go
+++ pkg/ingester/client/client.go
@@ -10,15 +10,17 @@ import (
 	_ "google.golang.org/grpc/encoding/gzip" // get gzip compressor registered
 
 	"github.com/weaveworks/common/middleware"
+	"flag"
 )
 
 type closableIngesterClient struct {
 	IngesterClient
 	conn *grpc.ClientConn
+	cfg Config
 }
 
 // MakeIngesterClient makes a new IngesterClient
-func MakeIngesterClient(addr string, timeout time.Duration, withCompression bool) (IngesterClient, error) {
+func MakeIngesterClient(addr string, timeout time.Duration, withCompression bool,cfg Config) (IngesterClient, error) {
 	opts := []grpc.DialOption{grpc.WithTimeout(timeout),
 		grpc.WithInsecure(),
 		grpc.WithUnaryInterceptor(grpc_middleware.ChainUnaryClient(
@@ -26,7 +28,7 @@ func MakeIngesterClient(addr string, timeout time.Duration, withCompression bool
 			middleware.ClientUserHeaderInterceptor,
 		)),
 		// We have seen 20MB returns from queries - add a bit of headroom
-		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(64 * 1024 * 1024)),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize( cfg.MaxRecvMsgSize)),
 	}
 	if withCompression {
 		opts = append(opts, grpc.WithDefaultCallOptions(grpc.UseCompressor("gzip")))
@@ -43,4 +45,13 @@ func MakeIngesterClient(addr string, timeout time.Duration, withCompression bool
 
 func (c *closableIngesterClient) Close() error {
 	return c.conn.Close()
+}
+
+
+type Config struct {
+	MaxRecvMsgSize int
+}
+
+func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
+	f.IntVar(&cfg.MaxRecvMsgSize,"ingester.client.max-recv-message-size",64*1024*1024,"Maximum message size, in bytes, this client will receive")
 }
